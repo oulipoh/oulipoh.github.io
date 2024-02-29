a40d73c7ef2034c9fd51722020df3e2de3a50ab2
Eyal Gruss <eyalgruss@gmail.com>
2024-02-29 17:58:12 +0200

    trying to fix mail icon take 4

--- resen/pages.js
+++ resen/pages.js
@@ -107,7 +107,7 @@ const kw_labels = {
 }
 
 const social = {
-    "mail": {"url": "mailto:", "label": "&#x2709;&#xfe0e;"},
+    "mail": {"url": "mailto:", "label": "&#x2709;"},  // don't add &#xfe0e; as it prevents targeting with CSS on iOS
     "web": {"label": "&#x1f3e0;&#xfe0e;"},
     "twitter": {"url": "twitter.com", "label": "&#x1f426;"},
     "github": {"url": "github.com", "label": "&#x1f431;"},
