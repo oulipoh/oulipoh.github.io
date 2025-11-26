1101cbe6dcf1b02438af627b0825e0a048d19d44
Eyal Gruss <eyalgruss@gmail.com>
2025-11-26 13:04:00 +0200

    no_fullscreen

--- resen/pages.js
+++ resen/pages.js
@@ -1007,5 +1007,5 @@ function toggle_fullscreen(event_or_elem, landscape=true, target_screen, elem) {
 }
 
 
-if (!document.body.requestFullscreen)  // For iPhone
-    document.body.classList.add('no_fullscreen')
+if (!document.documentElement.requestFullscreen)  // For iPhone
+    document.documentElement.classList.add('no_fullscreen')
